a0bef3c992b586f1b65cab24422564024c16c806
Israel Pérez Saucedo <neomatrixisra25@hotmail.com>
2017-06-02 06:29:02 -0500

    Avanzando en los laboratorios

---
+++ Lab10/Lab10/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using SALLab10;
+using Android.Content;
+
+namespace Lab10
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int counter = 0;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+            //var contentHeader = FindViewById<TextView>(Resource.Id.ContentHeader);
+            //contentHeader.Text = GetText(Resource.String.ContentHeader);
+
+            var clickMe = FindViewById<Button>(Resource.Id.ClickMe);
+            //var clickCounter = FindViewById<TextView>(Resource.Id.ClickCounter);
+
+            clickMe.Click += (s, e) =>
+            {
+                //counter++;
+                //clickCounter.Text = Resources.GetQuantityString(Resource.Plurals.numberOfClicks, counter, counter);
+
+                //var player = Android.Media.MediaPlayer.Create(this, Resource.Raw.sound);
+
+                //player.Start();
+
+                Intent intent = new Intent(this, typeof(ValidateActivity));
+                StartActivity(intent);
+            };
+
+            //Android.Content.Res.AssetManager Manager = this.Assets;
+
+            //using (var Reader = new System.IO.StreamReader(Manager.Open("Contenido.txt")))
+            //{
+            //    contentHeader.Text = $"\n\n{Reader.ReadToEnd()}";
+            //}
+        }
+    }
+}
+
---
+++ Lab10/Lab10/ValidateActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using SALLab10;
+
+namespace Lab10
+{
+    [Activity(Label = "@string/ApplicationName")]
+    public class ValidateActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Validate);
+
+            Button btnValidar = FindViewById<Button>(Resource.Id.ValidarButton);
+            EditText correoText = FindViewById<EditText>(Resource.Id.CorreoEditText);
+            EditText passwordText = FindViewById<EditText>(Resource.Id.PasswordEditText);
+
+            btnValidar.Click += async (sender, e) =>
+            {
+                try
+                {
+                    ServiceClient service = new ServiceClient();
+
+                    ResultInfo result = await service.ValidateAsync(correoText.Text, passwordText.Text,
+                        Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId));
+
+                    RunOnUiThread(() =>
+                    {
+                        FindViewById<TextView>(Resource.Id.EvidenciaTextView1).Text = $"{result.Status}\n{result.Fullname}\n{result.Token}";
+                    });
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, $"Error: {ex.Message}", ToastLength.Long).Show();
+                }
+            };
+        }
+    }
+}
